b21efe51629d7d740fc7b7d913179f02aadac3b0
OscarMartinez24 <oscarsaidmartineznavarro297@gmail.com>
2024-10-21 09:32:20 -0600

    Add files via upload

---
+++ P00Presentacion.php
@@ -0,0 +1,33 @@
+<?php 
+/*
+P00 Presentacion 
+Programa que contiene los datos personales 
+Martinez Navarro Oscar Said 
+3°A Programacion T.M.
+*/
+
+$N="Martinez Navarro Oscar Said";
+$D="16 Años";
+$E="CBTIS 89";
+$C="Durango, DGO";
+$CC="Programacion 3°A";
+
+echo "Mi nimbre es", $N;
+echo "<br>";
+echo "<br>";
+echo "Mi edad es ", $D;
+echo "<br>";
+echo "<br>";
+echo "Estudio en el ", $E;
+echo "<br>";
+echo "<br>";
+echo "Vivo en ", $C;
+echo "<br>";
+echo "<br>";
+echo "Lo que estudio es ", $CC;
+
+
+
+
+
+?>
